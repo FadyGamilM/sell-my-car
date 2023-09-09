1aa3536c1e2cec07799d54f2a01ec42101bb8921
FadyGamilM <1700961@eng.asu.edu.eg>
2023-09-09 22:45:26 +0300

    feature/finish the crud impl in the servie layer

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { PrimaryGeneratedColumn, Column, Entity } from "typeorm"
+import { log } from "console"
+import { PrimaryGeneratedColumn, Column, Entity, AfterInsert, AfterRemove, AfterUpdate } from "typeorm"
 
 @Entity()
 export class User {
@@ -10,4 +11,20 @@ export class User {
 
     @Column()
     password: string
+
+
+    @AfterInsert()
+    LogAfterInsert() {
+        log(`new user has been persisted with id = ${this.id} and email = ${this.email}`)
+    }
+
+    @AfterUpdate()
+    LogAfterUpdate() {
+        log(`user has been updated with id = ${this.id} and email = ${this.email}`)
+    }
+
+    @AfterRemove()
+    LogAfterRemove() {
+        log(`user has been removed with id = ${this.id} and email = ${this.email}`)
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,10 +10,45 @@ export class UsersService {
     ) { }
 
     // method to create and persist an User
-    Create(email: string, password: string) {
+    async Create(email: string, password: string) {
         const createdUser: User = this.repo.create({ email, password })
-        return this.repo.save(createdUser)
+        return await this.repo.save(createdUser)
+    }
+
+    // find user by id 
+    async FindByID(id: number) {
+        return await this.repo.findOneBy({ id: id })
+    }
+
+    // find user by email 
+    async FindByEmail(email: string) {
+        return await this.repo.findOneBy({ email: email })
+    }
+
+    // update user 
+    async UpdateByID(id: number, args: Partial<User>) {
+        // so we need to apply hooks to know which i
+        const userToUpdate = await this.FindByID(id)
+        if (!userToUpdate) {
+            throw new Error('user not found')
+        }
+        // update the props of this user by copying the props of userToUpdate instance first and then override via the properties that are present in the args instance
+        const updatedUser = { ...userToUpdate, ...args }
+        // call the save method on this updated instance 
+        return this.repo.save(updatedUser)
+    }
+
+    // remove user 
+    async DeleteByID(id: number) {
+        const userToDelete = await this.FindByID(id)
+        if (!userToDelete) {
+            throw new Error("user not found")
+        }
+
+        return await this.repo.remove(userToDelete)
     }
 }
 
 
+// this.repo.find({}) ==> will find all records that matches the given criteria
+// this.repo.findOne({}) ==> will find only one record that matches the given criteria
