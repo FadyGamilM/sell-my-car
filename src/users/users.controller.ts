7846ba7d88ab12fa4617f034d8317fb55c98b8f2
FadyGamilM <1700961@eng.asu.edu.eg>
2023-09-09 21:15:52 +0300

    feature/wire the typeorm repo with the user-service and user-controller for functional signup request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService: UsersService) { }
+
+    @Post("/signup")
+    Signup(@Body() req: CreateUserDto) {
+        return this.userService.Create(req.email, req.password)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User) private repo: Repository<User>
+    ) { }
+
+    // method to create and persist an User
+    Create(email: string, password: string) {
+        const createdUser: User = this.repo.create({ email, password })
+        return this.repo.save(createdUser)
+    }
+}
+
+
